cc789e0257ab4eec39d2201909755339c5182866
Arif Ahmed Begg <notes.aab.24@gmail.com>
2026-03-22 16:36:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ section.classList.add("hidden");
 document.getElementById(sectionId).classList.remove("hidden");
 
 }
+
+async function newQuote(){
+
+const response = await fetch("https://api.quotable.io/random");
+
+const data = await response.json();
+
+document.getElementById("quote-text").innerText = '"' + data.content + '"';
+
+document.getElementById("quote-author").innerText = "— " + data.author;
+
+}
+
+newQuote();
